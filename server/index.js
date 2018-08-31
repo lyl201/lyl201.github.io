500b30f7714dc4b56b6046648b0941ad1b8589c9
liyonglong <liyonglong@xiaomi.com>
2018-08-31 16:39:43 +0800

    a

--- server/index.js
+++ server/index.js
@@ -5,28 +5,17 @@ const Static = require('koa-static');
 const mongoose = require('mongoose');
 const router  = require('./router');
 const session = require('koa-session2');
-const formatResponse = require('./middleware/formatResponse')
+const formatterResponse = require('./middleware/formatterResponse')
 const fileUpload = require('./middleware/fileUpload');
 const app = new Koa();
-
-
- 
-
-
-
 const db = require('./config').db;
-
 mongoose.connect(db, { useNewUrlParser: true });
-
 const con = mongoose.connection;
-
 con.once('open', () => {
   
   console.log("数据库已成功连接");
-  const models = require('./models')(mongoose);
-   
+  const models = require('./models')(mongoose); 
   app.context.models = models;  //将模型的信息放到app中。
-
   // 支持跨域
   app.use(cors());
   // 保持登录
@@ -40,7 +29,7 @@ con.once('open', () => {
   // 支持路由
   app.use(router.routes());
   // 响应格式化
-  app.use(formatResponse())
+  app.use(formatterResponse())
 })
 
 con.on('error', console.error.bind(console, '连接数据库失败'));
