b024b974a3ccd91c49a6147bfc7c98c1a6e6d38b
liyonglong <liyonglong@xiaomi.com>
2018-10-15 19:48:35 +0800

    替换cdn

--- nginx/blog/static/js/5.7f49f3e127660d2a5334.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{"+B1k":function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var i={render:function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"detail-container"},[this._v("\n   comment\n")])},staticRenderFns:[]};var a=n("VU/8")({data:function(){return{}},computed:{},methods:{}},i,!1,function(t){n("iLD+")},"data-v-86382478",null);e.default=a.exports},"iLD+":function(t,e){}});
-//# sourceMappingURL=5.7f49f3e127660d2a5334.js.map
