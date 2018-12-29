6678e1f0bc9db3572157006556a57643e12284b5
李永龙 <liyonglong@xiaomi.com>
2018-12-29 10:17:25 +0800

    add group_2_copy_3_1546049843871.png

--- app.js
+++ app.js
@@ -49,7 +49,17 @@ const saveImg = async (req, res, {targetDir, repo, url, project}) => {
         await execAndConsole ('git pull');
         await execAndConsole ('git add .');
         await execAndConsole (`git commit -m "add ${imgName}"`);
-        await execAndConsole ('git push');
+        // await execAndConsole ('git push');
+        const {spawn} = require ('child_process');
+        const ls = spawn ('git', ['push']);
+
+        ls.stdout.on ('data', data => {
+          console.log (`输出：${data}`);
+        });
+
+        ls.stderr.on ('data', data => {
+          console.log (`错误：${data}`);
+        });
         res.writeHead (200, {
           'Content-Type': 'application/json;charset=UTF8',
         });
