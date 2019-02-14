c641a866abb91d0e15e8e60947e5e9426b351e57
李永龙 <liyonglong@xiaomi.com>
2019-02-14 10:48:39 +0800

    add grid6_2019-02-14_10-48-37-597.webp

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const githubAsImageServer = require('github-as-image-server');
 app.use(async (ctx, next) => {
 
   ctx.set('Access-Control-Allow-Origin', '*');
-  next();
+  await next();
 
 })
 app.use(
